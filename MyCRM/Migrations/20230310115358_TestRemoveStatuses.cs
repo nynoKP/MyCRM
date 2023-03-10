e38e07fcc7933723f5fb7fe8818f81a8514377e8
nynoK <snickers.888888888@gmail.com>
2023-03-10 16:54:25 +0500

    -----

---
+++ MyCRM/Migrations/20230310115358_TestRemoveStatuses.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestRemoveStatuses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskStatus_StatusId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatusId",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c40e32c7-ba16-40be-a0ff-3a8f47e37e88",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2b36b670-add2-43aa-ad56-cb6981fd5573", "AQAAAAIAAYagAAAAEDS66Lzosum9EglLtxzFvxGids39yGYruFRL70Vc5WKT/K+xwsY7e4sEVi8PAuTkLw==", "67ba0b54-1fe7-4aab-957c-5f529424c9d6" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskStatus_StatusId",
+                table: "Tasks",
+                column: "StatusId",
+                principalTable: "TaskStatus",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskStatus_StatusId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatusId",
+                table: "Tasks",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c40e32c7-ba16-40be-a0ff-3a8f47e37e88",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "1ef3851c-1154-4cb4-a44a-fcfbd24b3b5a", "AQAAAAIAAYagAAAAEOVkNHBa7LYi+azjjWWBc2WGDHhHcSD9i/M7ChzXMR/PKL4vNja27DhMuz+82JpAkQ==", "047b309c-b1ca-4c25-a17f-bc95a6964da4" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskStatus_StatusId",
+                table: "Tasks",
+                column: "StatusId",
+                principalTable: "TaskStatus",
+                principalColumn: "Id");
+        }
+    }
+}
